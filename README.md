c75888c775fedf865bcd2d97685cebc43fc2c812
AayushMathur03 <mathuraayush03@gmail.com>
2024-09-24 23:20:39 +0530

    uploading

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Baat_Chat
+# BaatChat
+
+An multi-person group chat app, displaying the significance of Chat in the current world where everyone is doing Baat (talking) over Chat, hence the name _**BaatChat**_.
+
+## Logo
+![Baatchat loho](https://github.com/user-attachments/assets/0c9d76a6-50ee-415c-8bbe-4e04830b9d97)
+
+## Introdution
+A Flutter App which have functionalities like Pop-Up Notification, login, & sign-up Authentication using Firebase function, messaging, SDK and many more.
+
+## Preview
+
+![Screenshot_1723039694](https://github.com/user-attachments/assets/6b0843f7-5fb1-4741-bb15-e9a5c7466423)
+
+![Screenshot_1723039700](https://github.com/user-attachments/assets/bcb9d048-a822-456f-b6da-0647beedb94a)
+
+![Screenshot_1723039705](https://github.com/user-attachments/assets/40abc44b-2f1e-4e32-8879-d21e85faa67b)
+
+![Screenshot_1723039707](https://github.com/user-attachments/assets/7e8db818-f42a-4331-9882-20a64cb33a68)
+
+![Screenshot_1723038109](https://github.com/user-attachments/assets/bf6897bd-e214-4a74-a5b9-a583fe290ad5)
+
+
+## Tech-Stack
+* Flutter
+* Dart
+* Firebase SDK
+* FireBase Cloud
---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:camera_gallery_image_picker/camera_gallery_image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key, required this.onPickImage});
+
+  final void Function(File pickedImage) onPickImage;
+
+  @override
+  State<UserImagePicker> createState() {
+    return _UserImagePickerState();
+  }
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImageFile;
+
+  void _pickImage() async {
+    final pickedImage = await CameraGalleryImagePicker.pickImage(
+      source: ImagePickerSource.both,
+      context: context,
+      cameraImageQuality: 50,
+      galleryImageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _pickedImageFile = File(pickedImage.path);
+    });
+
+    widget.onPickImage(_pickedImageFile!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage:
+              _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
